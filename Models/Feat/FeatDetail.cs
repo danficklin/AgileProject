959dab3c792a092dade201aabf5578d9f80b555e
Findley Griffiths <findleygriffiths@gmail.com>
2022-02-03 10:05:50 -0500

    Directories added

---
+++ Models/Feat/FeatDetail.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Models.Feat
+{
+    public class FeatDetail
+    {
+        public int FeatId { get; set; }
+        public string FeatName { get; set; }
+        public string FeatDescription { get; set; }
+    }
+}
