1388bffa1ce321c28c7c1af794fc56d83430f187
Findley Griffiths <findleygriffiths@gmail.com>
2022-02-03 14:45:01 -0500

    progress towards migration

--- Services/Feat/FeatService.cs
+++ Services/Feat/FeatService.cs
@@ -10,50 +10,50 @@ using Models.Feat;
 
 namespace Services.Feat
 {
-    public class FeatService : IFeatService
-    {
-        private readonly ApplicationDbContext _ctx;
-        public FeatService(ApplicationDbContext ctx)
-        {
-            _ctx = ctx;
-        }
-        public async Task<bool> CreateFeatAsync(FeatCreate model) // * Add a feat to the table
-        {
-            if (await GetFeatByNameAsync(model.Name)) return false;
-            var entity = new FeatEntity
-            {
-                Name = model.Name,
-                Type = model.Type,
-                ShortDescription = model.ShortDescription,
-                Prerequisites = model.Prerequisites,
-                Benefits = model.Benefits,
-                Special = model.Special,
-                Description = model.Description,
-                CombatFeat = model.CombatFeat,
-                DateAdded = DateTime.Now
-            };
-            _ctx.Feats.Add(entity);
-            var numberOfChanges = await _ctx.SaveChangesAsync();
-            return numberOfChanges == 1;
-        }
-        public async Task<FeatEntity> GetFeatByNameAsync(string name) // ? Get a feat by name helper method
-        {
-            return await _ctx.Feats.FirstOrDefaultAsync(feat => feat.Name.ToLower() == name.ToLower());
-        }
-        public async Task<IActionResult> GetAllFeatsAsync()
-        {
-            var feats = await _ctx.Feats.ToListAsync();
-            var featListItems = new List<FeatListItem>();
-            featListItems = feats.Select(f => new FeatListItem()
-            {
-                Id = f.Id,
-                Name = f.Name,
-                ShortDescription = f.ShortDescription,
-                CombatFeat = f.CombatFeat
-            }).ToList();
-            return Ok(featListItems);
+    // public class FeatService : IFeatService
+    // {
+    //     private readonly ApplicationDbContext _ctx;
+    //     public FeatService(ApplicationDbContext ctx)
+    //     {
+    //         _ctx = ctx;
+    //     }
+    //     public async Task<bool> CreateFeatAsync(FeatCreate model) // * Add a feat to the table
+    //     {
+    //         if (await GetFeatByNameAsync(model.Name)) return false;
+    //         var entity = new FeatEntity
+    //         {
+    //             Name = model.Name,
+    //             Type = model.Type,
+    //             ShortDescription = model.ShortDescription,
+    //             Prerequisites = model.Prerequisites,
+    //             Benefits = model.Benefits,
+    //             Special = model.Special,
+    //             Description = model.Description,
+    //             CombatFeat = model.CombatFeat,
+    //             DateAdded = DateTime.Now
+    //         };
+    //         _ctx.Feats.Add(entity);
+    //         var numberOfChanges = await _ctx.SaveChangesAsync();
+    //         return numberOfChanges == 1;
+    //     }
+    //     public async Task<FeatEntity> GetFeatByNameAsync(string name) // ? Get a feat by name helper method
+    //     {
+    //         return await _ctx.Feats.FirstOrDefaultAsync(feat => feat.Name.ToLower() == name.ToLower());
+    //     }
+    //     public async Task<IActionResult> GetAllFeatsAsync()
+    //     {
+    //         var feats = await _ctx.Feats.ToListAsync();
+    //         var featListItems = new List<FeatListItem>();
+    //         featListItems = feats.Select(f => new FeatListItem()
+    //         {
+    //             Id = f.Id,
+    //             Name = f.Name,
+    //             ShortDescription = f.ShortDescription,
+    //             CombatFeat = f.CombatFeat
+    //         }).ToList();
+    //         return Ok(featListItems);
 
-        }
+    //     }
 
-    }
+    // }
 }
