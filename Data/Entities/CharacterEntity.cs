09c7f1dc9cbf6e0dff6c8f3549a9b18c7b82a0f5
Findley Griffiths <findleygriffiths@gmail.com>
2022-02-03 13:47:21 -0500

    Pre-migration preparation

--- Data/Entities/CharacterEntity.cs
+++ Data/Entities/CharacterEntity.cs
@@ -10,17 +10,29 @@ namespace Data.Entities
     {
         [Key]
         public int Id { get; set; }
+
         [Required]
+        [MinLength(2, ErrorMessage = "{0} Must be at least {1} characters long.")]
+        [MaxLength(50, ErrorMessage = "{0} Must be at most {1} characters long.")]
         public string Name { get; set; }
+
         [Required]
+        [MinLength(2, ErrorMessage = "{0} Must be at least {1} characters long.")]
+        [MaxLength(50, ErrorMessage = "{0} Must be at most {1} characters long.")]
         public string Class { get; set; }
+
         [Required]
+        [MinLength(2, ErrorMessage = "{0} Must be at least {1} characters long.")]
+        [MaxLength(50, ErrorMessage = "{0} Must be at most {1} characters long.")]
         public string Race { get; set; }
+
         [Required]
         public int Level { get; set; }
+
         [Required]
         public Dictionary<string, int> Attributes { get; set; }
-        [Required]
+
+        
         public string Backstory { get; set; }
         //public List<Feat> Feats { get; set; }
         //public Group GroupMembership { get; set; }
