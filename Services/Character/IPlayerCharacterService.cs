2b5c73a9174a1356ac5aa6c0999b7e56a37d2dd7
Alan Murugan <alanmurugan@gmail.com>
2022-02-04 10:03:21 -0500

    PlayerCharacter/IPlayercharacter Services added

---
+++ Services/Character/IPlayerCharacterService.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Models.Character;
+
+namespace Services.Character
+{
+    public interface IPlayerCharacterService
+    {
+        Task<bool> CreatePlayerCharacterAsync(PlayerCharacterCreate request);
+        Task<IEnumerable<PlayerCharacterList>> GetAllCharactersAsync();
+        Task<PlayerCharacterDetail> GetNoteByIdAsync(int playerCharacterId);
+        Task<bool> UpdateCharacterAsync(PlayerCharacterUpdate request);
+        Task<bool> DeleteCharacterAsync(int playerCharacterId);
+    }
+}
